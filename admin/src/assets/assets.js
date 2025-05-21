c8e8850fb6dd798d5da4f34a532ab336ff0a6e41
Raj Kumar <145471187+raj123215@users.noreply.github.com>
2025-05-21 19:43:58 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'https://food-delivery-backand-unuc.onrender.com'
+export const url = 'https://food-delivery-backend-unuc.onrender.com'
 export const currency = '₹'
 
 export const assets ={
